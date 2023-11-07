a59455e5e877179086a87d1bc98bfc59cf66e9e7
Çağdaş Güleç <102041399+Cagdas-Gulec@users.noreply.github.com>
2023-11-07 22:07:50 +0300

    Initial commit.

---
+++ Source/Horror_Game/Horror_GameGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Horror_GameGameModeBase.h"
+
---
+++ Source/Horror_Game/Horror_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Horror_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORROR_GAME_API AHorror_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
